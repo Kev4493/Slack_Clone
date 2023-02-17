7263fb3174eedcca5da07f3590802e8f757a03c9
Kevin Wagner <kevin_wagner4493@web.de>
2023-02-17 09:02:40 +0100

    update

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
@@ -13,10 +14,11 @@ export class RegisterComponent {
 
   registerForm: FormGroup;
   firebaseErrorMessage: any;
+  generatedUserColor: string;
 
   hide = true;
 
-  constructor(private authService: AuthService, private router: Router, private afAuth: AngularFireAuth) { 
+  constructor(private authService: AuthService, private router: Router, private afAuth: AngularFireAuth, private firestore: AngularFirestore) { 
     this.firebaseErrorMessage = '';
   }
 
@@ -50,10 +52,36 @@ export class RegisterComponent {
         user.updateProfile({                                                    // Über die "updateProfile"-Methode des Benutzers wird der "displayName" auf den Wert aus dem Formular gesetzt.
           displayName: this.registerForm.value.displayName
         }).then(() => {
+          this.generateUserColor();
+          this.sendUserToDb()
           this.router.navigate(['/home']);                                      // Wenn die Aktualisierung des Profils erfolgreich ist, wird der Benutzer zur Seite "/home" weitergeleitet.
         });
       });
     });
   }
-  
+
+
+  generateUserColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    this.generatedUserColor = color
+  }
+
+
+  sendUserToDb() {
+    this.afAuth.user.subscribe((userDb) => {                                    // Die Funktion verwendet das "afAuth.user" Objekt und abonniert es mit einem Callback.
+      if (!userDb) return;                                                      // Innerhalb des Callbacks wird überprüft, ob ein Benutzer vorhanden ist. Wenn nicht, wird die Funktion beendet! 
+      this.authService.user.userName = userDb.displayName;
+      this.authService.user.userEmail = userDb.email;
+      this.authService.user.userId = userDb.uid;
+      this.authService.user.userColor = this.generatedUserColor;
+
+      this.firestore
+      .collection('registeredUser')
+      .add(this.authService.user.toJSON())
+    });
+  }  
 }
--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,13 +1,11 @@
 export class User {
     userName: string;
-    userInitials: string;
     userEmail: string;
     userId: string;
     userColor: string;
 
     constructor(obj?: any) { 
         this.userName = obj ? obj.userName : '';
-        this.userInitials = obj ? obj.userInitials : '';
         this.userEmail = obj ? obj.userEmail : '';
         this.userId = obj ? obj.userId : '';
         this.userColor = obj ? obj.userColor : '';
@@ -16,7 +14,6 @@ export class User {
     public toJSON() {
         return {
             userName: this.userName,
-            userInitials: this.userInitials,
             userEmail: this.userEmail,
             userId: this.userId,
             userColor: this.userColor,
